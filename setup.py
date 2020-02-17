a1beb411d6ad90f64ce984bffee36631e8fd0d51
Ashton Sidhu <sidhu@pop-os.localdomain>
2020-02-17 14:23:05 -0500

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An autonomous webscraping tool that uses ML to scrape text from any website.',
+    author='Ashton Sidhu',
+    license='BSD-3',
+)
